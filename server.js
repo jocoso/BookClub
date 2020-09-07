95ce0614a7ad2709a84416dc5ac9766a833252e8
jocoso <joshua.collado022@gmail.com>
2020-09-07 17:21:57 -0400

    removed morgan packaged due to problems

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const { createServer } = require('http');
 const express = require('express');
 const compression = require('compression');
-const morgan = require('morgan');
 const path = require('path');
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000)
@@ -22,7 +21,6 @@ if(!dev) {
 
 if(dev) {
   app.set("port", PORT);
-  app.use(morgan('dev'));
   app.use(express.static(path.resolve(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
