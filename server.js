367a880d3a110f332a880aac928e712c30354885
jocoso <joshua.collado022@gmail.com>
2020-09-07 17:26:28 -0400

    delete some morgan calls I forgot

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const dev = app.get('env') !== 'production';
 if(!dev) {
   app.disable('x-powered-by');
   app.use(compression());
-  app.use(morgan('common'));
 
   app.use(express.static(path.resolve(__dirname, 'build')));
 
